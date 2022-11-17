702cc862067ef8d96f43692cc728419a891a4153
ahk1221 <ahmed16.ahk@gmail.com>
2022-11-18 00:13:44 +0500

    Show time taken and level on end screen

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def send_data(time, puzzle_level):
 def main():
     won = False
     completed = False
+    time_taken = None
     time_completed = datetime(1, 1, 1)
 
     while True:
@@ -74,7 +75,6 @@ def main():
                     print(globals.start_time)
                 #pass
 
-
         main_screen.fill(BLACK)
 
         if not completed and globals.active_game_index < len(screens) and screens[globals.active_game_index] != None:
@@ -97,22 +97,21 @@ def main():
                 time_completed = datetime.now()
                 time_taken = time_completed - globals.start_time
 
-                text = get_font(50).render(str(time_taken)[2:7], 1, (255,255,255))
-                level_text = get_font(50).render('Level ' + str(globals.active_game_index), 1, (255,255,255))
-                main_screen.blit(text, (10, 10))
-                main_screen.blit(level_text, (10, text.get_height() + 5))
-
                 if int(time_taken.total_seconds()) >= 60 * 60:
                     completed = True
                     send_data(time_taken.total_seconds(), globals.active_game_index)
 
-        if won:
-            main_screen.fill((0,0,0))
+        if globals.active_game_index != 0 and time_taken != None:
+            text = get_font(50).render(str(time_taken)[2:7], 1, (255,255,255))
+            level_text = get_font(50).render('Level ' + str(globals.active_game_index), 1, (255,255,255))
+            main_screen.blit(text, (10, 10))
+            main_screen.blit(level_text, (10, text.get_height() + 5))
+
+        if won:     
             gameWonText = get_font(50).render("You Won", True, (255, 255, 255))
             main_screen.blit(gameWonText, gameWonText.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)))
 
         elif completed:
-            main_screen.fill((0,0,0))
             gameOverText = get_font(50).render("Game over, get lost", 1, (255, 255, 255))
             main_screen.blit(gameOverText, (SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
 
